85a20744a18d99598df2801b096fe620d549a5b3
yuanjunling <261412489@qq.com>
2020-02-21 15:55:41 +0800

    menuv

---
+++ components/index/menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav">
+      <dt>全部分类</dt>
+      <dd v-for="(item, idx) in menu" :key="idx">
+        <i :class="item.type" />{{ item.name }}<span class="arrow" />
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script >
+export default {
+  data () {
+    return {
+      menu: [{
+        type: 'food',
+        name: '美食'
+      },
+      {
+        type: 'takeout',
+        name: '外卖'
+      },
+      {
+        type: 'hotel',
+        name: '酒店'
+      },
+      {
+        type: 'movie',
+        name: '电影院'
+      }
+      ]
+    }
+  },
+}
+</script>
+
+
